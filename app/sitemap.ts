e8fc2a4769f41e05be4f67bbff5975886c31b739
olafoyeku josiah <olafoyekujosiah@gmail.com>
2026-09-12 22:38:57 +0100

    changed sitemap details

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://seagatelounge.com';
+  const baseUrl = 'https://seagate-lounge-and-bar.vercel.app';
   
   return [
     {
